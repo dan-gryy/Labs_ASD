80dc493af1ed849f33dcf82d46e2bfd168e43113
dan-gryy <dan.gryschenko@gmail.com>
2025-09-23 22:11:04 +0300

    first commit

---
+++ lab1/lab1-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+
+    printf("Введіть значення x: ");
+    scanf("%i", &x);
+
+    if (x >= 0)
+        if (x < 7)
+            y = -6 * x * x + 8;
+        else
+            printf("Функція не визначена для цього x\n");
+    else if (x < -10)
+        y = -x * x * x / 7 + 10;
+    else if (x >= 11)
+        y = -x * x * x / 7 + 10;
+    else
+        printf("Функція не визначена для цього x\n");
+    if ((x >= 0 && x < 7) || (x < -10) || (x >= 11))
+        printf("y = %i\n", y);
+}
