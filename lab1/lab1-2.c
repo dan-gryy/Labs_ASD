b9c20f3499a35ffb5b9e35f392908df0db612e92
dan-gryy <dan.gryschenko@gmail.com>
2025-09-23 22:28:18 +0300

    small changes

--- lab1/lab1-2.c
+++ lab1/lab1-2.c
@@ -8,16 +8,13 @@ int main()
     printf("Введіть значення x: ");
     scanf("%i", &x);
 
-    if (x >= 0)
-        if (x < 7)
-            y = -6 * x * x + 8;
-        else
-            printf("Функція не визначена для цього x\n");
-    else if (x < -10)
-        y = -x * x * x / 7 + 10;
-    else if (x >= 11)
+    if (x >= 0 && x < 7) 
+        y = -6 * x * x + 8;
+    else if (x < -10 || x >= 11)
         y = -x * x * x / 7 + 10;
     else
         printf("Функція не визначена для цього x\n");
+
+    if ((x >= 0 && x < 7) || (x < -10) || (x >= 11))
         printf("y = %i\n", y);
 }
