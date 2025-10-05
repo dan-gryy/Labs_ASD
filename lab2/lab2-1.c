2a81cd8d15131ff0bfcfa210e6d4fb101cbc58fe
dan-gryy <dan.gryschenko@gmail.com>
2025-10-05 23:58:57 +0300

    first commit

---
+++ lab2/lab2-1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, count = 0;
+    printf("Type n: ");
+    scanf("%d", &n);
+
+    double P = 1.0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        double a = i * sqrt(i);
+        count += 2;
+        double b = 0.0;
+
+        for (int j = 1; j <= i; j++)
+        {
+            b += j * sin(j) + j;
+            count += 3;
+        }
+
+        if (b != 0)
+        {
+            P *= a / b;
+            count += 2;
+        }
+    }
+
+    printf("Result P = %.7lf\n", P);
+    printf("Number of operation: %d\n", count);
+    return 0;
+}
---
+++ lab2/lab2-2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int n;
+    printf("Type n: ");
+    scanf("%d", &n);
+    double P = 1.0;
+    int function = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        double S = 0.0;
+        for (int j = 1; j <= i; j++)
+        {
+            S += j * sin(j) + j;
+            function += 3;
+        }
+        P *= (i * sqrt(i) / S);
+        function += 3;
+    }
+
+    printf("%.7lf\n", P);
+    printf("Number of functions: %d\n", function);
+    return 0;
+}
